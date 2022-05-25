057d1c8988d51f47bd2cb2f870c105f91aae1272
Роман Соколов <sokoloff.rv@gmail.com>
2022-05-25 18:53:39 +0700

    Исправил вызов функции count_tasks

--- templates/new_task.php
+++ templates/new_task.php
@@ -6,7 +6,7 @@
             <?php foreach ($projects as $project): ?>
             <li class="main-navigation__list-item <?php if (($selected_project_id) === $project['id']):?>main-navigation__list-item--active<?php endif;?>">
                 <a class="main-navigation__list-item-link" href="/index.php?project_id=<?= $project['id'] ?>"><?= htmlspecialchars($project['title']) ?></a>
-                <span class="main-navigation__list-item-count"><?= count_tasks($all_tasks, $project['title']); ?></span>
+                <span class="main-navigation__list-item-count"><?= count_tasks($connect, $project['title'], $user_id); ?></span>
             </li>
             <?php endforeach; ?>
         </ul>
