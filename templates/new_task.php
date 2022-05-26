5bc9a01ddccd9bece99136a08c2244e71eacfc2d
Роман Соколов <sokoloff.rv@gmail.com>
2022-05-26 11:11:43 +0700

    Исправил ошибку на страницах нового проекта/задачи

--- templates/new_task.php
+++ templates/new_task.php
@@ -4,7 +4,7 @@
     <nav class="main-navigation">
         <ul class="main-navigation__list">
             <?php foreach ($projects as $project): ?>
-            <li class="main-navigation__list-item <?php if ($selected_project_id === $project['id']):?>main-navigation__list-item--active<?php endif;?>">
+            <li class="main-navigation__list-item">
                 <a class="main-navigation__list-item-link" href="/index.php?project_id=<?= $project['id'] ?>"><?= htmlspecialchars($project['title']) ?></a>
                 <span class="main-navigation__list-item-count"><?= count_tasks($connect, $project['title'], $user_id); ?></span>
             </li>
