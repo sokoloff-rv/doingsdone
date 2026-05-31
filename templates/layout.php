414959e767563400a31e29ad87249f28c47f6191
Roman Sokolov <sokoloff.rv@gmail.com>
2026-05-31 18:07:42 +0600

    Экранирование имени пользователя в шапке для защиты от XSS

--- templates/layout.php
+++ templates/layout.php
@@ -30,7 +30,7 @@
                         <a class="main-header__side-item button button--plus open-modal" href="add.php">Добавить задачу</a>
                         <div class="main-header__side-item user-menu">
                             <div class="user-menu__data">
-                                <p><?= $user_name ?></p>
+                                <p><?= htmlspecialchars($user_name) ?></p>
                                 <a href="/logout.php">Выйти</a>
                             </div>
                         </div>
