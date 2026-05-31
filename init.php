62923c8884a827dceee2b68909e7903504ab814b
Roman Sokolov <sokoloff.rv@gmail.com>
2026-05-31 17:33:48 +0600

    Отключен вывод ошибок на экран, включено их логирование

--- init.php
+++ init.php
@@ -1,7 +1,8 @@
 <?php
-error_reporting(E_ALL); 
-ini_set('display_errors', 1); 
-ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+ini_set('log_errors', 1);
 
 require_once('helpers.php');
 require_once('functions.php');
