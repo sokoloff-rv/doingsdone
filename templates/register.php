92707ccb65b8f315cb2f7728c0630dd4bc05c4d1
Roman Sokolov <sokoloff.rv@gmail.com>
2026-05-31 18:10:52 +0600

    Пароль больше не возвращается в значение поля при ошибке формы

--- templates/register.php
+++ templates/register.php
@@ -16,7 +16,7 @@
         <div class="form__row">
             <label class="form__label" for="password">Пароль <sup>*</sup></label>
 
-            <input class="form__input <?= isset($errors['password']) ? "form__input--error" : ""?>" type="password" name="password" id="password" value="<?= htmlspecialchars(get_post_value('password')) ?>" placeholder="Введите пароль">
+            <input class="form__input <?= isset($errors['password']) ? "form__input--error" : ""?>" type="password" name="password" id="password" placeholder="Введите пароль">
             <?php if (isset($errors['password'])): ?>
                 <p class="form__message"><?= $errors['password'] ?></p>
             <?php endif; ?>
